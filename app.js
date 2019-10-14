9e025f2913feac18e565d66ccdfb9e582f9b3645
lemonoink <jianghaotian126@126.com>
2019-10-14 13:47:33 +0800

    ok

--- app.js
+++ app.js
@@ -2,19 +2,8 @@ const http = require('http');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
-
-
-
-function showHtml(htmlPath, res) {
-    fs.readFile(htmlPath, (err, data) => {
-        if (!err) {
-            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.end(data);
-        } else {
-            console.log(err);
-        }
-    })
-}
+const queryString = require('querystring');
+let {chapterList, userList} = require('./data');
 
 http.createServer((req, res) => {
     let urlObj = url.parse(req.url);
@@ -68,9 +57,90 @@ http.createServer((req, res) => {
             res.end();
             break;
         // ----- 功能实现 ----- //
-
-
-
+        // 获取文章列表
+        case '/getChapterList':
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(chapterList));
+            break;
+        // 获取一篇文章
+        case '/getDetail':
+            var chapterId = queryString.parse(urlObj.query).chapterId;
+            var dataList = [];
+            chapterList.forEach((data, index) => {
+                if (data.chapterId == chapterId) {;
+                    dataList.push(data);
+                }
+            })
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(dataList));
+            break;
+        // 删除文章
+        case '/delChapter':
+            var chapterId = queryString.parse(urlObj.query).chapterId;
+            for (var i = 0; i < chapterList.length; i++) {
+                if (chapterList[i].chapterId == chapterId) {
+                    chapterList.splice(i, 1);
+                    data = {code: 0};
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.end(JSON.stringify(data));
+                    return ;
+                }
+            }
+            data = {code: -1};
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(data));
+            break;
+        // 登录
+        case '/getlogin':
+            var postData = "";
+            req.on("data", function (chunk) {
+                postData += chunk;
+            });
+            req.on("end", function () {
+                var params = queryString.parse(postData);
+                var username = params.username;
+                var password = params.password;
+                var i = 0;
+                for (i = 0; i < userList.length; i++) {
+                    if(userList[i].username == username && userList[i].pwd == password) {
+                        data = {code: 0};
+                        res.writeHead(200, {'Content-Type': 'application/json'});
+                        res.end(JSON.stringify(data));
+                        return ;
+                    }
+                }
+                data = {code: -1};
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(data));
+            });
+            break;
+            // 添加文章
+            case '/add':
+                var postData = "";
+                req.on("data", function (chunk) {
+                    postData += chunk;
+                });
+                req.on("end", function () {
+                    var params = queryString.parse(postData);
+                    var title = params.title;
+                    var content = params.content;
+                    var date = new Date();
+                    var dict = {
+                        "chapterId": chapterList[chapterList.length-1].chapterId + 1,
+                        "chapterName": title,
+                        "imgPath": "",
+                        "chapterDes": content,
+                        "chapterContent": content,
+                        "publishTimer": `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`,
+                        "author": "admin",
+                        "views": 0
+                    }
+                    chapterList.push(dict);
+                    data = {code: 0};
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.end(JSON.stringify(data));
+                });
+                break;
         // ----- 静态资源 ----- //
         default:
             var htmlPath = path.join(__dirname, 'src', urlObj.pathname);
@@ -83,9 +153,9 @@ http.createServer((req, res) => {
                     if (extName === '.js') {
                         res.writeHead(200, {'Content-Type': 'text/javascript'});
                     } else if (extName === '.css') {
-                        res.writeHead(200, {'Content-Type': 'text/css'});                        
+                        res.writeHead(200, {'Content-Type': 'text/css'});
                     } else if (extName === '.png') {
-                        res.writeHead(200, {'Content-Type': 'image/png'});                        
+                        res.writeHead(200, {'Content-Type': 'image/png'});
                     } else if (extName === '.jpg' || extName === '.jpeg') {
                         res.writeHead(200, {'Content-Type': 'image/jpeg'});
                     } else {
@@ -100,3 +170,17 @@ http.createServer((req, res) => {
     }
 }).listen(8083);
 console.log("server listen 8083")
+
+// ----- function ----- //
+function showHtml(htmlPath, res) {
+    fs.readFile(htmlPath, (err, data) => {
+        if (!err) {
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end(data);
+        } else {
+            console.log(err);
+        }
+    })
+}
+
+
